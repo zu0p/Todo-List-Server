06ebd480329781933ed23e9d8d2289be0968baac
[zu0p] <a33a66a99@naver.com>
2020-06-01 22:16:02 +0900

    Update login api
    - 로그인 id, pw, isLogged를 json으로 보내줌

--- route/user.js
+++ route/user.js
@@ -45,6 +45,11 @@ router.post('/check_id_duplication', function(req, res){
 router.post('/login', function(req, res){
     var user_id = req.body.id;
     var user_pw = req.body.pw;
+    var user = {
+        id: user_id,
+        pw: user_pw,
+        isLogged: 0
+    };
 
     connection.query("SELECT COUNT (user_id) AS count FROM user WHERE user_id = '"+user_id+"' AND user_pw = '"+user_pw+"'",
         function(err, result, fidleds){
@@ -52,13 +57,16 @@ router.post('/login', function(req, res){
                 res.send('err: ' + err);
             else {                
                 console.log(result[0].count);
+                console.log(result);
 
-                if(result[0].count == 1) 
-                    res.send('success');
+                if(result[0].count == 1){
+                    // res.send('success');
+                    user.isLogged = 1;
+                    res.json(user);
+                    // res.end(JSON.stringify(user));
+                }
                 else    
                     res.send('fail');
-                //-> 프론트에서 status==200이면 로그인
-
             }
         });
 });
